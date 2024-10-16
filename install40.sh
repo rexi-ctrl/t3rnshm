bba1416b962f637a3cec91a622e51b0716046feb
Nendrik <118098449+nendrik@users.noreply.github.com>
2024-10-17 02:35:27 +0700

    Update install40.sh

--- install40.sh
+++ install40.sh
@@ -9,8 +9,8 @@ sudo apt -q update
 sudo apt -qy upgrade
 
 
-EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.21.5/executor-linux-v0.21.5.tar.gz"
-EXECUTOR_FILE="executor-linux-v0.21.5.tar.gz"
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.21.8/executor-linux-v0.21.8.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.21.8.tar.gz"
 
 echo "Downloading the Executor binary from $EXECUTOR_URL..."
 curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
@@ -44,7 +44,6 @@ export RPC_ENDPOINTS_ARBT='https://arb-sepolia.g.alchemy.com/v2/V57W5XD-wVcublXo
 export RPC_ENDPOINTS_BSSP='https://base-sepolia.g.alchemy.com/v2/V57W5XD-wVcublXoMUC-Exv0wuNQARoO'
 export RPC_ENDPOINTS_BLSS='https://blast-sepolia.g.alchemy.com/v2/V57W5XD-wVcublXoMUC-Exv0wuNQARoO'
 export RPC_ENDPOINTS_OPSP='https://opt-sepolia.g.alchemy.com/v2/V57W5XD-wVcublXoMUC-Exv0wuNQARoO'
-export RPC_ENDPOINTS_l1rn='https://brn.rpc.caldera.xyz/http'
 
 
 sleep 2
